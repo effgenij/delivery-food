eef42e9f88574f201f8df96064d76dbc867afca9
effgenij <efgenij@gmail.com>
2020-05-05 13:26:57 +0300

    Added authorization

--- js/main.js
+++ js/main.js
@@ -8,3 +8,76 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+//Authorization
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth')
+const logInForm = document.querySelector('#logInForm');
+const logInInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('deliveryFood');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open')
+};
+
+
+
+function authorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem('deliveryFood');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+
+    checkAuth();
+  };
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  buttonOut.addEventListener('click', logOut);
+};
+
+function notAuthorized() {
+  function logIn(event) {
+    event.preventDefault();
+    login = logInInput.value;
+    if (login === '') {
+      alert ('Логин не может быть пустым');
+      toggleModalAuth();
+      login = logInInput.value;
+    };
+    
+    localStorage.setItem('deliveryFood', login);
+
+    toggleModalAuth();
+    buttonAuth.removeEventListener('click', toggleModalAuth);
+    closeAuth.removeEventListener('click', toggleModalAuth);
+    logInForm.removeEventListener('submit', logIn);
+    logInForm.reset();
+    checkAuth();
+  };
+  
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+};
+
+function checkAuth() {
+  if (login) {
+    authorized();
+  } else {
+    notAuthorized();
+  };
+};
+
+checkAuth();
